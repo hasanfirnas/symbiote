ae6fdcfcfc55f83f7ce448e05106ee93bc9347c4
Hasan Firnas <63351774+hasanfirnas@users.noreply.github.com>
2023-01-29 20:32:03 +0530

    Update Checks.py

--- Checks.py
+++ Checks.py
@@ -1,6 +1,6 @@
 import subprocess
 import ctypes
-#import requests 
+import requests 
 import urllib
 #from urllib import urlopen
 from os import system, getuid, path
@@ -25,71 +25,19 @@ def net():
 def verCheck():
     #system('clear')
     print("\n{0}[{2}#{0}] {2}Checking For Updates{2}...".format(RED, WHITE, CYAN, GREEN, DEFAULT , YELLOW ))
-    system('wget -q -O test.txt https://raw.githubusercontent.com/404-ghost/symbiote/master/version.txt')
-    #system('clear')
-    file = open('version.txt','r')
-    a = file.read()
-    x = a.split("\n")
-    file2 = open('test.txt','r')
-    b = file2.read()
-    z = b.split("\n")
-    file.close()
-    file2.close()
-    if x[0] == z[0]:
-        print("{0}[{2}#{0}] {2}[Up-To-Date]- {0}v {6}{4}".format(RED, WHITE, CYAN, GREEN, DEFAULT , YELLOW, z[0]))
-        system('git checkout HEAD^ CapturedData --quiet && git checkout HEAD^ pross_kill.py --quiet && git checkout HEAD^ Server --quiet && git checkout HEAD^ LICENSE --quiet && git checkout HEAD^ Checks.py --quiet && git checkout HEAD^ logo.py --quiet && git checkout HEAD^ makepath.py --quiet && git checkout HEAD^ symbiote.py --quiet && git checkout HEAD^ version.txt --quiet')
-        system('git stash --quiet')
-        system('git pull --quiet')
-        system('rm -rf test.txt')
-        sleep(2)
+    response = requests.get('https://raw.githubusercontent.com/hasanfirnas/symbiote/master/version.txt')
+    print(response.strip())
+    with open("version.txt", "r") as f:
+        local_version = f.read().strip()
+    if local_version[0] == response:
+        print("{0}[{2}#{0}] {2}[Up-To-Date]- {0}v {6}{4}".format(RED, WHITE, CYAN, GREEN, DEFAULT , YELLOW, response))
+        system('git fetch --quiet; git reset --hard origin/master --quiet; git pull --quiet')
+        sleep(1)
     else:
         print("\n{0}[{2}#{0}] {2}Their Is A Newer Version Available.".format(RED, WHITE, CYAN, GREEN, DEFAULT , YELLOW))
-        print("{0}[{2}#{0}] {0}[{2}Current{0}]{2}- {0}v {6}\n{0}[{2}#{0}] {0}[{2}Available{0}]{2}- {0}v.{7}".format(RED, WHITE, CYAN, GREEN, DEFAULT, YELLOW, x[0], z[0])) 
-        print("{0}[{2}#{0}] {2}Updating To The Latest Version {0}[{2}v {6}{0}] \n{0}[{2}#{0}] {2}Please Wait....{7}\n".format(RED, WHITE, CYAN, GREEN, DEFAULT , YELLOW, z[0] ,GREEN2))
-##            system('rm -rf symbiote.py && wget https://raw.githubusercontent.com/404-ghost/symbiote/master/symbiote.py')
-##            system('clear')
-##            system('rm -rf Checks.py && wget https://raw.githubusercontent.com/404-ghost/symbiote/master/Checks.py')
-##            system('clear')
-##            system('rm -rf logo.py && wget https://raw.githubusercontent.com/404-ghost/symbiote/master/logo.py')
-##            system('clear')
-##            system('rm -rf makepath.py && wget https://raw.githubusercontent.com/404-ghost/symbiote/master/makepath.py') 
-##            system('clear')
-##            system('rm -rf Server/www_b/face.jpg && cd Server/www_b/ && wget https://raw.githubusercontent.com/404-ghost/symbiote/master/Server/www_b/face.jpg')
-##            system('rm -rf Server/www_b/index.php && cd Server/www_b/ && wget https://raw.githubusercontent.com/404-ghost/symbiote/master/Server/www_b/index.php')
-##            system('clear')
-##            system('rm -rf Server/www_b/index2.html && cd Server/www_b/ && wget https://raw.githubusercontent.com/404-ghost/symbiote/master/Server/www_b/index2.html')
-##            system('rm -rf Server/www_b/ip.php && cd Server/www_b/ && wget https://raw.githubusercontent.com/404-ghost/symbiote/master/Server/www_b/ip.php')
-##            system('clear')
-##            system('rm -rf Server/www_b/post.php && cd Server/www_b/ && wget https://raw.githubusercontent.com/404-ghost/symbiote/master/Server/www_b/post.php')
-##            system('rm -rf Server/www_b/worldmap.jpg && cd Server/www_b/ && wget https://github.com/404-ghost/symbiote/blob/master/Server/www_b/worldmap.jpg')
-##            system('clear')
-##                   
-##            system('rm -rf Server/www_f/face.jpg && cd Server/www_f/ && wget https://raw.githubusercontent.com/404-ghost/symbiote/master/Server/www_f/face.jpg')
-##            system('rm -rf Server/www_f/index.php && cd Server/www_f/ && wget https://raw.githubusercontent.com/404-ghost/symbiote/master/Server/www_f/index.php')
-##            system('clear')
-##            system('rm -rf Server/www_f/index2.html && cd Server/www_f/ && wget https://raw.githubusercontent.com/404-ghost/symbiote/master/Server/www_f/index2.html')
-##            system('rm -rf Server/www_f/ip.php && cd Server/www_f/ && wget https://raw.githubusercontent.com/404-ghost/symbiote/master/Server/www_f/ip.php')
-##            system('clear')
-##            system('rm -rf Server/www_f/post.php && cd Server/www_f/ && wget https://raw.githubusercontent.com/404-ghost/symbiote/master/Server/www_f/post.php')
-##            system('rm -rf Server/www_f/worldmap.jpg && cd Server/www_f/ && wget https://github.com/404-ghost/symbiote/blob/master/Server/www_f/worldmap.jpg')
-##            system('clear')
-##            system('rm -rf version.txt && wget https://raw.githubusercontent.com/404-ghost/symbiote/master/version.txt')
-##            system('clear')
-            #system("git clean -d -f > /dev/null && git pull -f > /dev/null")
-        system('git checkout HEAD^ CapturedData --quiet && git checkout HEAD^ pross_kill.py --quiet && git checkout HEAD^ Server --quiet && git checkout HEAD^ LICENSE --quiet && git checkout HEAD^ Checks.py --quiet && git checkout HEAD^ logo.py --quiet && git checkout HEAD^ makepath.py --quiet && git checkout HEAD^ symbiote.py --quiet && git checkout HEAD^ version.txt --quiet')
-        system('git stash --quiet')
-        system('git pull')
-        sleep(1)
-        system('rm -rf test.txt')
-        file = open('version.txt','r')
-        a = file.read()
-        x = a.split("\n")      
-        print("{0}[{2}*{0}] {2}Version Status After Update.{2}.\n".format(RED, WHITE, CYAN, GREEN, DEFAULT , YELLOW))
-        print("{0}[{2}*{0}] {0}[{2}Current{0}]{2}- {0}v {6}\n{0}[{2}*{0}] {0}[{2}Available{0}]{2}- {0}v.{7}{4}".format(RED, WHITE, CYAN, GREEN, DEFAULT , YELLOW, x[0], z[0]))
-        sleep(1)
-        system('clear')
-        print("\n\n\n\t\t{2}[{0}#{2}] {0}Restart program \n {2}Enter this command to run {0}-> {3}python3 symbiote.py".format(RED, WHITE, CYAN, GREEN, DEFAULT , YELLOW))
+        system('git fetch --quiet; git reset --hard origin/master --quiet; git pull --quiet')
         exit()
+
 def checkjp2a():
     system('clear')
     if 256 != system('which jp2a > /dev/null'):
@@ -163,7 +111,7 @@ def checkLocalxpose():
         print(' {0}[{2}*{0}]{2} Downloading Localxpose...{5}'.format(RED, WHITE, CYAN, GREEN, DEFAULT ,YELLOW))
         if 'Android' in str(check_output(('uname', '-a'))) or 'arm' in str(check_output(('uname', '-a'))):
             filename = 'loclx-linux-arm.zip'
-            url = 'https://api.localxpose.io/api/v2/downloads/'+filename
+            url = 'https://lxpdownloads.sgp1.digitaloceanspaces.com/cli/'+filename
             req=system('wget {0}'.format(url))
             #with open("{0}", "wb".format(filename)) as file_obj:
             #file_obj.write(req.content)
@@ -178,7 +126,7 @@ def checkLocalxpose():
                 filename = 'loclx-linux-amd64.zip'.format(ostype)
             else:
                 filename = 'loclx-linux-386.zip'.format(ostype)
-        url = 'https://api.localxpose.io/api/v2/downloads/'+filename
+        url = 'https://lxpdownloads.sgp1.digitaloceanspaces.com/cli/'+filename
         req=system('wget {0}'.format(url))
         system('unzip {0} && rm {0}'.format(filename))
         system('mv loclx Server/')
@@ -204,7 +152,7 @@ def loadingHack():
             else:
                 r = ""
             sys.stdout.write('\r'+chainehack+r)
-            time.sleep(0.006)
+            time.sleep(0.06)
 
 def loadingTextPrint():
     string ="                    "+"[*]"+" Starting symbiote......"
@@ -215,7 +163,7 @@ def loadingTextPrint():
         x = 1
         a += 1
         while x <= len(string):
-            times = "0.01"
+            times = "0.1"
             times += str(random.choice(range(1, 3)))
             sys.stdout.write("\r "+string[:x]+">")
             time.sleep(float(times))
